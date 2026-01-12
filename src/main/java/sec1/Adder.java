869564763eb7842700fccc6c3279b7aab1441ec2
arunbababa <hatuki.1.gzs@icloud.com>
2026-01-12 13:34:02 +0900

    first commit: 初期設定済み

---
+++ src/main/java/sec1/Adder.java
@@ -0,0 +1,9 @@
+package sec1;
+
+public class Adder {
+
+    // 足し算メソッド　これに対してテストを書いてみる
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/sec1/AdderTest.java
@@ -0,0 +1,7 @@
+package sec1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdderTest {
+
+}
